356d4c98b8d2b30949c7e29a70f897fa7464a03b
linjiansi <pmon.ken.1467@gmail.com>
2022-12-18 21:25:07 +0900

    added an install file

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -u
+
+THIS_DIR=$(cd $(dirname $0); pwd)
+
+cd $THIS_DIR
+
+echo "start setup..."
+
+for f in .??*; do
+  [ "$f" = ".git" ] && continue
+
+  ln -snfv ~/dotfiles"$f" ~/
+done
+
+cat << EOF
+**************************************************
+DOTFILES SETUP FINISHED! bye.
+**************************************************
+EOF
